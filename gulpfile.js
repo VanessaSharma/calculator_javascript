ab152621328d93b887beb97a0b0afe777d389e5d
Vanessa Palacios Sharma and laura krouse <vanessa+laura@>
2016-10-10 11:17:26 -0700

    added calculator javascript into gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 'use strict';
 var gulp = require('gulp');
-gulp.task("hello", function() {
-  console.log("hello!");
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+
+gulp.task("concatScripts", function() {
+  gulp.src(['js/scripts.js', 'js/jquery-3.1.1.js', 'js/calculator-interface.js'])
+  .pipe(concat("js/app.js"))
+  .pipe(gulp.dest("js"));
 });
 
+gulp.task("minifyScripts", function() {
+  gulp.src("js/app.js")
+  .pipe(uglify())
+  .pipe(rename('app.min.js'))
+  .pipe(gulp.dest('js'));
+});
 
-gulp.task("default", ["hello"]) {
+gulp.task("default", ["hello"], function() {
   console.log("this is a default");
 });
